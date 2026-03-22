f88a5c4a51eacce348c9fd55555e2390021b4e46
Port Zhang <achui_1980@163.com>
2026-03-22 21:37:28 +0800

    fix: add missing QDialog import

--- ui.py
+++ ui.py
@@ -32,6 +32,7 @@ from PyQt6.QtWidgets import (
     QSizePolicy,
     QGridLayout,
     QProgressDialog,
+    QDialog,
 )
 from PyQt6.QtCore import Qt, QSize, QMetaObject, QThread
 from PyQt6.QtGui import QIcon, QFont, QAction
